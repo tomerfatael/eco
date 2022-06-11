e368ef17a6d9d9597e43892cd8514e83dbe1541c
tomerfatael <tomerfatael@mail.tau.ac.il>
2022-06-12 00:22:41 +0300

    q15

--- eco.R
+++ eco.R
@@ -89,13 +89,13 @@ q12_data$residuals<-q12_model$residuals
 q12_data$residuals_squared<-q12_data$residuals^2
 q14_model<-lm(residuals_squared ~ elig + post + elig*post, data=q12_data)
 summary(q14_model)
-coeftest(q14_model, vcov = vcovHC(q14_model, type="HC1"))
+coeftest(q12_model, vcov = vcovHC(q12_model, type="HC1"))
 
 #q15
-q15_model = lm(hs ~ elig + post + nsibs + any_col + ageimmig + singlemom + fem + elig*post , data=q12_data)
+q15_model = lm(hs ~ elig + post + nsibs + any_col + ageimmig + singlemom + fem + elig*post, data=q12_data)
 summary(q15_model)
-q15_model_effects = plm(hs ~ elig + post + nsibs + any_col + ageimmig + singlemom + fem + elig*post + factor(statefip), index="statefip", model="within", data=q12_data)
-summary(q15_model_effects)
 q12_data$residuals_squared_full = q15_model$residuals^2
+q15_model_check = lm(residuals_squared_full ~ elig + post + nsibs + any_col + ageimmig + singlemom + fem + elig*post, data=q12_data)
+summary(q15_model_check)
 coeftest(q15_model, vcov = vcovHC(q15_model, type="HC1"))
 
